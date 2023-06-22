1bedfaaca57e517dca3d6fbcee0e2a989ffa126f
dfunani <dfunani@live.co.za>
2023-06-22 19:33:52 +0200

    Done

--- src/scripts/microapp.js
+++ src/scripts/microapp.js
@@ -72,8 +72,8 @@ function startPaymentOverlay(title) {
 
 function onPaymentStatusChanged(transactionId, status, error) {
   let res = `Transaction ID: ${transactionId}, Status: ${status}, Error: ${error}`;
-  const regSuccess = new RegExp("success*", "i");
-  const regProgress = new RegExp("*progress*", "i");
+  const regSuccess = new RegExp("success", "i");
+  const regProgress = new RegExp("progress", "i");
   const toastElem = document.getElementById("toast_" + TITLE);
   const toastBody = document.getElementById("toast_body_" + TITLE);
   const toast = new bootstrap.Toast(toastElem);
